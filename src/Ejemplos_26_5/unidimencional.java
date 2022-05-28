54ce2d88e83dbc5b5eb3d6c0a5c018e12c143350
JavierGD15 <3057448870301@ingenieria.usac.edu.gt>
2022-05-27 19:41:55 -0600

    arrays terminados

--- src/Ejemplos_26_5/unidimencional.java
+++ src/Ejemplos_26_5/unidimencional.java
@@ -6,18 +6,43 @@ public class unidimencional {
 
 	public static void main(String[] args) {
 		
+		//Mostrar en la primera fila Nombre, apellido, telefono, cumpleaños, dpi y luego los datos
+		String array [][] = new String [5][5];
+		array [0][0] ="Nombre";
+		array [0][1] ="Apellido";
+		array [0][2] ="Telefono";
+		array [0][3] ="Cumpleaños";
+		array [0][4] ="DPI";
 		
-		int array [][] = new int [8][8];
-		int x = 64;
+		array [1][0] ="Joel";
+		array [1][1] ="Cos";
+		array [1][2] ="48751636";
+		array [1][3] ="18/01/98";
+		array [1][4] ="3014526699778";
+		
+		array [2][0] ="Junior";
+		array [2][1] ="Florian";
+		array [2][2] ="48751636";
+		array [2][3] ="18/01/90";
+		array [2][4] ="3014526699647";
+		
+		array [3][0] ="Joel";
+		array [3][1] ="Cos";
+		array [3][2] ="48751636";
+		array [3][3] ="18/01/98";
+		array [3][4] ="3014526699778";
+		
+		array [4][0] ="Junior";
+		array [4][1] ="Florian";
+		array [4][2] ="48751636";
+		array [4][3] ="18/01/90";
+		array [4][4] ="3014526699647";
 		
 		for (int i = 0; i < array.length; i++) {
 			
 			for (int j = 0; j < array[i].length; j++) {
 				
-				array[i][j] = x;
-				x--;
-				
-				System.out.print(array[i][j]);
+				System.out.print("|"+array[i][j]+"|");
 				
 			}
 			System.out.println("");
---
+++ src/Práctica/juego.java
@@ -0,0 +1,10 @@
+package Práctica;
+
+public class juego {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
